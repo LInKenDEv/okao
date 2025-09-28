d5f2c5a868afff9312da4bb47fd7831643b6423b
LInKenDEv <dogsaternos@gmail.com>
2025-09-28 20:07:59 +0800

    Update and rename Test.js to ApirSyn

--- Apir/Extensions/Test.js
+++
@@ -1,9 +0,0 @@
-// @name Hello World
-// @description Shows a toast that says "Hello from extension!"
-// @version 1.0.0
-// @author You
-
-export default function runExtension({ showToast }) {
-  showToast("Hello from extension!");
-}
-
